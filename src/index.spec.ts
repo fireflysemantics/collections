f1528a34eae98617ffc9be30897929e3695cf15d
Ole Ersoy <ole.ersoy@gmail.com>
2019-02-28 20:25:40 -0600

    Switched from Mocha to Jest

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,3 @@
-import { expect } from "chai";
-import "mocha";
 import {
   push,
   pull,
@@ -11,14 +9,15 @@ import {
   sortNumbersAscending,
   sortStringsDescending,
   sortStringsAscending,
-  reverse
+  reverse,
+  deepClone
 } from "./index";
 
 describe("Push", () => {
   it("should have four elements post push", () => {
     const original = ["a", "b"];
     const result = ["a", "b", "c", "d"];
-    expect(push(original, "c", "d")).to.eql(result);
+    expect(push(original, "c", "d")).toEqual(result);
   });
 });
 
@@ -26,7 +25,7 @@ describe("Pull", () => {
   it("should have 4 elements post pull", () => {
     const original = ["a", "b"];
     const result = ["c", "d", "a", "b"];
-    expect(pull(original, "c", "d")).to.eql(result);
+    expect(pull(original, "c", "d")).toEqual(result);
   });
 });
 
@@ -34,7 +33,7 @@ describe("Pre", () => {
   it("should have four elements after pre", () => {
     const original = ["a", "b"];
     const result = ["c", "d", "a", "b"];
-    expect(pre(original, ["c", "d"])).to.eql(result);
+    expect(pre(original, ["c", "d"])).toEqual(result);
   });
 });
 
@@ -42,7 +41,7 @@ describe("Post", () => {
   it("should have four elements after post", () => {
     const original = ["a", "b"];
     const result = ["a", "b", "c", "d"];
-    expect(post(original, ["c", "d"])).to.eql(result);
+    expect(post(original, ["c", "d"])).toEqual(result);
   });
 });
 
@@ -50,7 +49,7 @@ describe("Remove", () => {
   it("should have 1 element post remove", () => {
     const original = ["a", "b"];
     const result = ["a"];
-    expect(remove(original, 1)).to.eql(result);
+    expect(remove(original, 1)).toEqual(result);
   });
 });
 
@@ -58,7 +57,7 @@ describe("Pop", () => {
   it("should have 1 element post pop", () => {
     const original = ["a", "b"];
     const result = ["a"];
-    expect(pop(original)).to.eql(result);
+    expect(pop(original)).toEqual(result);
   });
 });
 
@@ -66,7 +65,7 @@ describe("SortNumbersDescending", () => {
   it("should sort numbers in descending order", () => {
     const original = [10, 20];
     const result = [20, 10];
-    expect(sortNumbersDescending(original)).to.eql(result);
+    expect(sortNumbersDescending(original)).toEqual(result);
   });
 });
 
@@ -74,7 +73,7 @@ describe("SortNumbersAscending", () => {
   it("should sort numbers in ascending order", () => {
     const original = [20, 10];
     const result = [10, 20];
-    expect(sortNumbersAscending(original)).to.eql(result);
+    expect(sortNumbersAscending(original)).toEqual(result);
   });
 });
 
@@ -82,13 +81,13 @@ describe("SortStringsAscending", () => {
   it("should sort Strings in ascending order", () => {
     let original = ["Beta", "Alpha"];
     let result = ["Alpha", "Beta"];
-    expect(sortStringsAscending(original)).to.eql(result);
+    expect(sortStringsAscending(original)).toEqual(result);
     original = ["alpha", "Alpha"];
     result = ["Alpha", "alpha"];
-    expect(sortStringsAscending(original)).to.eql(result);
+    expect(sortStringsAscending(original)).toEqual(result);
     original = ["a", "A"];
     result = ["A", "a"];
-    expect(sortStringsAscending(original)).to.eql(result);
+    expect(sortStringsAscending(original)).toEqual(result);
   });
 });
 
@@ -96,20 +95,32 @@ describe("SortStringsDescending", () => {
   it("should sort strings in descending order", () => {
     let original = ["Alpha", "Beta"];
     let result = ["Beta", "Alpha"];
-    expect(sortStringsDescending(original)).to.eql(result);
+    expect(sortStringsDescending(original)).toEqual(result);
 
     original = ["Alpha", "alpha"];
     result = ["alpha", "Alpha"];
-    expect(sortStringsDescending(original)).to.eql(result);
+    expect(sortStringsDescending(original)).toEqual(result);
   });
 });
 
-
 describe("Reverse", () => {
-    it("should reverse the array", () => {
-      const original = [20, 10];
-      const result = [10, 20];
-      expect(reverse(original)).to.eql(result);
-    });
+  it("should reverse the array", () => {
+    const original = [20, 10];
+    const result = [10, 20];
+    expect(reverse(original)).toEqual(result);
   });
-  
+});
+
+describe("Deep Clone", () => {
+  class Todo {
+    constructor() {}
+  }
+
+  it("should deep clone the array", () => {
+    const original = [new Todo(), new Todo()];
+    const result = deepClone(original);
+    expect(deepClone(original)).toEqual(result);
+    expect(original[0]===result[0]).toBeFalsy();
+    expect(original[1]===result[1]).toBeFalsy();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -187,3 +187,26 @@ export function sortStringsDescending(array: string[]): string[] {
 export function reverse<T>(array: T[]): T[] {
   return [...array].reverse();
 }
+
+/**
+ * Deep clones an array of instances of type T and returns a new array.
+ * @param array The array to deep clone
+ * @return A new deep clone of the array argument
+ *
+ * Note that all the instances being cloned must implement this type of 
+ * constructor:
+ * <pre>
+ * constructor(obj: any) {
+ * Object.assign(this, obj);
+ * </pre>
+ * 
+ * @example
+<pre>
+  const original = [new Todo(), new Todo()];
+  const result = [new Todo(), new Todo()];
+  expect(deepClone(original)).to.eql(result);
+</pre>
+*/
+export function deepClone(array: any[]): any[] {
+  return array.map((e: any) => new e.constructor(e));
+}
