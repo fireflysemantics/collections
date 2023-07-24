c898d4160c1e8354f257926dc0cfded7eadf8b43
Ole Ersoy <ole.ersoy@gmail.com>
2023-07-24 15:31:26 -0400

    Added remainder to modulus action function

--- projects/fs-collections/src/lib/src/modulus-action.function.ts
+++ projects/fs-collections/src/lib/src/modulus-action.function.ts
@@ -5,16 +5,18 @@
  * @param divisor The divisor.
  * @param onMatch Whether to perform the function on match.
  * @param f The function.
+ * @param remainder The remainder that triggers the match, defaults to zero.
  * @returns The data array with the modifications made by the function.
  */
 export function modulusAction<E>(
   data: E[],
   divisor: number,
   onMatch: boolean = true,
-  f: (data: E) => E
+  f: (data: E) => E,
+  remainder: number = 0
 ): E[] {
   data.forEach((item: E, index: number) => {
-    const match: boolean = index % divisor === 0;
+    const match: boolean = index % divisor === remainder;
     if (onMatch && match) {
       f(item);
     } else if (!match) {
